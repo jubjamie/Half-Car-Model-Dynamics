c279277068a0ad6f97f5fe4f9a415e4a068b76ef
Jamie Cooke <jamie@iamjamiecooke.com>
2018-12-17 21:07:54 +0000

    Add and implement willLoad and initModel to slx model

--- willLoad.m
+++ willLoad.m
@@ -1,15 +1,15 @@
-answer = questdlg('Save Workspace?', ...
+answer = questdlg('Load Workspace?', ...
 	'Save Variables in Workspace to file?', ...
 	'Yes','No','Yes');
 % Handle response
 switch answer
     case 'Yes'
         prompt = {'Filename'};
-        title = 'Save Workspace';
+        title = 'Load Workspace';
         dims = [1 35];
         definput = {'baseVehicleData'};
-        saveName = inputdlg(prompt,title,dims,definput);
-        save(saveName{1});
+        loadName = inputdlg(prompt,title,dims,definput);
+        load(loadName{1});
     case 'No'
-        disp('Not saving Workspace Variables')
+        disp('Not saving Workspace Variables');
 end
