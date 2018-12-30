be45bb8043413343998969f620720d07f8a22fd3
Jamie Cooke <jamie@iamjamiecooke.com>
2018-12-30 16:10:54 +0000

    Add relative forcing plot.

--- graphs/graphsMaker.m
+++ graphs/graphsMaker.m
@@ -7,6 +7,12 @@ plot(Displacements.Body_Displacement.Time,Displacements.Body_Displacement.Data,'
 hold on;
 plot(Displacements.Front_Offset.Time,Displacements.Front_Offset.Data+Displacements.Body_Displacement.Data,'r-');
 plot(Displacements.Back_Offset.Time,Displacements.Back_Offset.Data+Displacements.Body_Displacement.Data,'b-');
+
+% Overlay relative road profile forcing
+wheelSettle=-((m1+m2)*g)/kT;
+bodySettle=(-m1*g/ks)+wheelSettle;
+
+plot(Forcing.Time,Forcing.Data+bodySettle,'g--');
 set(f11,'Position',[250 250 900 450]);
 hold off;
 grid on;
