a7e38a6cfd3cf81be2abff3b334688356093e24c
Jamie Cooke <jamie@iamjamiecooke.com>
2018-12-30 16:07:34 +0000

    Make basic car displacement graph

---
+++ graphs/graphsMaker.m
@@ -0,0 +1,16 @@
+% Make graphs for report - Half Body
+
+%% Car Displacement Replica
+clf(11);
+f11=figure(11);
+plot(Displacements.Body_Displacement.Time,Displacements.Body_Displacement.Data,'k-');
+hold on;
+plot(Displacements.Front_Offset.Time,Displacements.Front_Offset.Data+Displacements.Body_Displacement.Data,'r-');
+plot(Displacements.Back_Offset.Time,Displacements.Back_Offset.Data+Displacements.Body_Displacement.Data,'b-');
+set(f11,'Position',[250 250 900 450]);
+hold off;
+grid on;
+legend('CoM Displacement','Front Point Displacement','Rear Point Displacement');
+xlabel('Time (s)');
+ylabel('Displacement (m)');
+title('Car Point Displacements with Overlaid Relative Forcing');
