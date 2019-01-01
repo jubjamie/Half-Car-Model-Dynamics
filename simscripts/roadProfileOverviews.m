9e0df5378ab0797806abbdbcc53d8012ff3ea31c
Jamie Cooke <jamie@iamjamiecooke.com>
2019-01-01 18:55:31 +0000

    Change speeds in batch sim runnings

--- simscripts/roadProfileOverviews.m
+++ simscripts/roadProfileOverviews.m
@@ -1,10 +1,15 @@
 % Run all the sims to generate the basic graphs for the different road
 % profiles
-
+speeds=[5,10,15];
 for pt=1:5
-   disp(['Running Sim ' num2str(pt)]);
-   set_param('halfmodel/Forcing Selector/Road Profile Control Signal','Value',num2str(pt))
-   sim('halfmodel');
-   graphsMaker;
+   for sp=1:numel(speeds)
+    disp(['Running Sim ' num2str(pt) ' @ ' num2str(speeds(sp)) ' m/s']);
+    set_param('halfmodel/Forcing Selector/Road Profile Control Signal','Value',num2str(pt));
+    v_speed=speeds(sp);
+    sim('halfmodel');
+    simStatus=get_param('halfmodel','SimulationStatus');
+    waitfor(simStatus,'Value','stopped');
+    graphsMaker;
+   end
 end
 disp('Sims Complete');
