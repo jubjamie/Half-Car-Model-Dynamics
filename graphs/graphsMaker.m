803892dfce0c5cf49e15b974ee910313e3a17e3b
Jamie Cooke <jamie@iamjamiecooke.com>
2019-01-01 16:48:30 +0000

    Add wheel displacement graph

--- graphs/graphsMaker.m
+++ graphs/graphsMaker.m
@@ -1,4 +1,6 @@
 % Make graphs for report - Half Body
+%Get settings
+roadProfileType=
 
 %% Car Displacement Replica
 f11=figure(11);
@@ -20,3 +22,18 @@ xlabel('Time (s)');
 ylabel('Displacement (m)');
 title('Car Point Displacements with Overlaid Relative Forcing');
 saveas(f11,'graphs/carDisplacementMulti.png');
+
+%% Wheel Displacements
+f12=figure(12);
+plot(Wheels_Data.Front_Wheel_Displacement.Time,Wheels_Data.Front_Wheel_Displacement.Data,'r-');
+hold on;
+plot(Wheels_Data.Rear_Wheel_Displacement.Time,Wheels_Data.Rear_Wheel_Displacement.Data,'b-');
+
+set(f12,'Position',[250 250 900 450]);
+hold off;
+grid on;
+legend('Front Wheel Displacement','Rear Wheel Displacement');
+xlabel('Time (s)');
+ylabel('Displacement (m)');
+title('Wheel Displacements');
+saveas(f12,'graphs/wheelDisplacementMulti.png');
