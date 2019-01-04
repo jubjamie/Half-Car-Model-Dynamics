afccb4450f917ef674171a36729ab1d7e7eb14c5
Jamie Cooke <jamie@iamjamiecooke.com>
2019-01-04 14:50:58 +0000

    Further Sin study

--- simscripts/sinRoads.m
+++ simscripts/sinRoads.m
@@ -1,9 +1,11 @@
 % Run the sin case at different speeds
 % Set speeds and profile type.
-speeds=0.5:0.1:30;
+speeds=0.5:0.25:25;
 set_param('halfmodel/Forcing Selector/Road Profile Control Signal','Value','2');
 
 amps=zeros(1,numel(speeds));
+frontAmps=amps;
+rearAmps=amps;
 velAmps=amps;
 frontVelAmps=amps;
 rearVelAmps=amps;
@@ -14,6 +16,8 @@ for sp=1:numel(speeds)
     simStatus=get_param('halfmodel','SimulationStatus');
     waitfor(simStatus,'Value','stopped');
     amps(sp)=sinStats(Displacements.Body_Displacement,5);
+    frontAmps(sp)=sinStats(Displacements.Front_Combined_Displacements,5);
+    rearAmps(sp)=sinStats(Displacements.Rear_Combined_Displacements,5);
     velAmps(sp)=sinStats(Displacements.Body_Velocity,5);
     frontVelAmps(sp)=sinStats(Displacements.Front_Combined_Velocity,5);
     rearVelAmps(sp)=sinStats(Displacements.Rear_Combined_Velocity,5);
@@ -23,9 +27,13 @@ disp('Sims Complete');
 f21=figure(21);
 hold off;
 plot(speeds,amps);
+hold on;
+plot(speeds,frontAmps);
+plot(speeds,rearAmps);
 xlabel('Vehicle Speed (m/s)');
 ylabel('Average Body Displacement Amplitude (m/s)');
 title('Average Body Displacement Amplitude on the Sinousoidal Road at Different Speeds');
+legend('CoM','Front','Rear');
 grid on;
 set(f21,'Position',[150 150 800 550]);
 saveas(f21,'graphs/sin/averageAmps.png');
