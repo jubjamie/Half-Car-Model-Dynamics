57216514a0c764ae954803e2ad8067b8fa8493e5
Jamie Cooke <jamie@iamjamiecooke.com>
2019-01-04 22:11:57 +0000

    Complete verification

--- initModel.m
+++ initModel.m
@@ -10,10 +10,9 @@ m1=250; % kg - Mass per point
 
 % Suspension
 ks=2e4; % Suspension spring stiffness
-x0=0.2; % Spring Hardening distance
 
-csExtend=1200; % dampener rate under extension
-csCompress=600; % dampener rate under compression
+cs=1000; % dampener rate under extension
+% dampener rate under compression
 
 % Wheel
 m2=20; % kg - Wheel mass
